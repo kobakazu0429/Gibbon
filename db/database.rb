31c53b1fed7e0f4b0d016fa7bbee698863756627
kobakakzu0429 <kobakazu10@gmail.com>
2019-07-15 20:54:22 +0900

    fix database.rb

--- db/database.rb
+++ db/database.rb
@@ -7,8 +7,4 @@ require "erb"
 
 Dotenv.load
 
-class Base < ActiveRecord::Base
-  databases = YAML.load(ERB.new(File.read('config/database.yml')).result)
-  establish_connection(databases[Rails.env])
-  self.abstract_class = true
-end
+ActiveRecord::Base.configurations = YAML.load(ERB.new(File.read('config/database.yml')).result)
