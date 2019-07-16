a2f77cc56a4bd62f543439d17c8e62333808d69a
kobakakzu0429 <kobakazu10@gmail.com>
2019-07-16 16:59:28 +0900

    add validations

--- models/book.rb
+++ models/book.rb
@@ -1,4 +1,8 @@
 require_relative "./application_record.rb"
 
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :volume, presence: true, numericality: { only_integer: true }
+  validates :isCompleted, inclusion: { in: [true, false] }
 end
