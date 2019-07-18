f14e31eee7c4b049d23335b68915b1a803868606
kobakakzu0429 <kobakazu10@gmail.com>
2019-07-18 09:36:20 +0900

    create book_params()

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -8,14 +8,14 @@ class Api::V1::BooksController < Api::V1::BaseController
   end
 
   post "/", provides: :json do
-    Book.create(JSON.parse(request.body.read))
+    Book.create(book_params(JSON.parse(request.body.read)))
 
     redirect '/api/v1/books/'
   end
 
   put "/:id/", provides: :json do
     @book = Book.find(params["id"])
-    @book.update(JSON.parse(request.body.read))
+    @book.update(book_params(JSON.parse(request.body.read)))
 
     redirect '/api/v1/books/'
   end
@@ -24,4 +24,15 @@ class Api::V1::BooksController < Api::V1::BaseController
     Book.find(params["id"]).destroy
     redirect '/api/v1/books/'
   end
+
+  private
+    def book_params(params)
+      params.symbolize_keys.slice(
+        :title,
+        :author,
+        :volume,
+        :isCompleted,
+        :note
+      )
+    end
 end
