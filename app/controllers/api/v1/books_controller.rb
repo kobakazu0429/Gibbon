9e9d4d654e771636b13e9f6d5225d60c932bb13e
kobakakzu0429 <kobakazu10@gmail.com>
2019-07-18 09:50:48 +0900

    add error_handling

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -8,14 +8,21 @@ class Api::V1::BooksController < Api::V1::BaseController
   end
 
   post "/", provides: :json do
-    Book.create(book_params(JSON.parse(request.body.read)))
-
+    begin
+      Book.create!(book_params(JSON.parse(request.body.read)))
+    rescue ActiveRecord::RecordInvalid => e
+      pp e.record.errors
+    end
     redirect '/api/v1/books/'
   end
 
   put "/:id/", provides: :json do
     @book = Book.find(params["id"])
-    @book.update(book_params(JSON.parse(request.body.read).except(:id)))
+    begin
+      @book.update!(book_params(JSON.parse(request.body.read).except(:id)))
+    rescue ActiveRecord::RecordInvalid => e
+      pp e.record.errors
+    end
 
     redirect '/api/v1/books/'
   end
