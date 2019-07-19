716c862f323f27f022a6a2c6e118d422fb8dedc8
kobakakzu0429 <kobakazu10@gmail.com>
2019-07-20 01:07:25 +0900

    [wip] api from frontend

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -3,6 +3,10 @@ require_relative "../../application_controller.rb"
 module Api
   module V1
     class BaseController < ApplicationController
+      before do
+        headers 'Access-Control-Allow-Origin' => '*'
+        headers 'Access-Control-Allow-Headers' => 'Origin, X-Requested-With, Content-Type, Accept'
+      end
     end
   end
 end
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => <p>Hello, React World !</p>;
+const App = () => {
+  const [books, setBooks] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const res = await axios.get("http://localhost:9090/api/v1/books/");
+
+      setBooks(res.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      {books.map(v => (
+        <p key={v.id}>
+          {v.author} - {v.title}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 export default App;
